c644901dee73f0cef84fa1a99c48a262f762c7cb
Artem <artemmishenko@mail.ru>
2019-11-20 16:50:43 +0400

    попытка смены страницы

--- app/controllers/app.go
+++ app/controllers/app.go
@@ -2,6 +2,7 @@ package controllers
 
 import (
 	"github.com/revel/revel"
+	"pm/app/routes"
 )
 
 type App struct {
@@ -9,5 +10,10 @@ type App struct {
 }
 
 func (c App) Index() revel.Result {
+	return c.Render()
+}
+
+func (c App) Employees() revel.Result {
+	return c.Redirect(routes.App.Employees())
 	return c.Render()
 }
